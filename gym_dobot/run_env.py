8db478370746064edb473f515ed6a75b61c247dd
WarrG3X <warrier.abhishek@gmail.com>
2018-07-12 11:43:31 +0530

    Minor Bug Fix in run_env.py

--- gym_dobot/run_env.py
+++ gym_dobot/run_env.py
@@ -6,10 +6,10 @@ from scipy.misc import imsave
 
 @click.command()
 @click.option('--env', default="DobotPickAndPlaceEnv", help='Which environment to run (Eg. - DobotReachEnv)')
-@click.option('--render',default=True,help='Whether to render the environment')
+@click.option('--render',default=1,help='Whether to render the environment')
 @click.option('--steps',default=100,help='Number of timesteps to run the environment each time')
 @click.option('--clutter',default=20,help='Number of clutter objects for clutter environments')
-@click.option('--rand_dom',default=False,help='Whether to use domain randomization')
+@click.option('--rand_dom',default=0,help='Whether to use domain randomization')
 def main(env,render,steps,clutter,rand_dom):
     if 'Clutter' in env:
         env = getattr(envs,env)(clutter_num=clutter,rand_dom=rand_dom)
