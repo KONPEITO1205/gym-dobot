940907507c8dc950ea998337a26293ec0f412abf
WarrG3X <warrier.abhishek@gmail.com>
2019-02-09 20:39:31 +0530

    Renamed Environments for compatibility with gym.make()

--- gym_dobot/__init__.py
+++ gym_dobot/__init__.py
@@ -1,6 +1,6 @@
 from gym.envs.registration import register
 
-for reward_type in ['sparse', 'dense']:
+for reward_type in ['sparse']:
     suffix = 'Dense' if reward_type == 'dense' else ''
     kwargs = {
         'reward_type': reward_type,
@@ -8,35 +8,35 @@ for reward_type in ['sparse', 'dense']:
 
 
 register(
-    id='DobotReach{}-v0'.format(suffix),
+    id='DobotReach-v1'.format(suffix),
     entry_point='gym_dobot.envs:DobotReachEnv',
     kwargs=kwargs,
     max_episode_steps=50,
 )
 
 register(
-    id='DobotPush{}-v0'.format(suffix),
+    id='DobotPush-v1'.format(suffix),
     entry_point='gym_dobot.envs:DobotPushEnv',
     kwargs=kwargs,
     max_episode_steps=50,
 )
 
 register(
-    id='DobotPickAndPlace{}-v0'.format(suffix),
+    id='DobotPickAndPlace-v1'.format(suffix),
     entry_point='gym_dobot.envs:DobotPickAndPlaceEnv',
     kwargs=kwargs,
     max_episode_steps=50,
 )
 
 register(
-    id='DobotClutterPickAndPlace{}-v0'.format(suffix),
+    id='DobotClutterPickAndPlace-v1'.format(suffix),
     entry_point='gym_dobot.envs:DobotClutterPickAndPlaceEnv',
     kwargs=kwargs,
     max_episode_steps=50,
 )
 
 register(
-    id='DobotClutterPush{}-v0'.format(suffix),
+    id='DobotClutterPush-v1'.format(suffix),
     entry_point='gym_dobot.envs:DobotClutterPushEnv',
     kwargs=kwargs,
     max_episode_steps=50,
